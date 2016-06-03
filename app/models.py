1be96617778b1afa8573d0eebdab25c44fc4bd95
Julia Herron <julia.herron@locusenergy.com>
2016-06-02 17:02:08 -0700

    starting user logins

--- app/models.py
+++ app/models.py
@@ -1,10 +1,29 @@
 from app import db
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(64), index=True, unique=True)  # column type(max length), index, uniqueness
     email = db.Column(db.String(120), index=True, unique=True)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return True
+
+    def get_id(self):
+        try:
+            return unicode(self.id)
+        except NameError:
+            return str(self.id)
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)  # print method, for debugging
@@ -17,4 +36,4 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
-        return '<Post %r>' %r (self.body)
+        return '<Post %r>' % (self.body)

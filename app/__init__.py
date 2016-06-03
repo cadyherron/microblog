b57c95a2c3a2803123e730617918b5f2959e3bb5
Julia Herron <julia.herron@locusenergy.com>
2016-06-03 09:58:21 -0700

    user logins

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
-from flask.ext.login import LoginManager
-from flask.ext.openid import OpenID
+from flask_login import LoginManager
+from flask_openid import OpenID
 from config import basedir
 
 app = Flask(__name__)
@@ -11,6 +11,7 @@ db = SQLAlchemy(app)
 
 lm = LoginManager()
 lm.init_app(app)
+lm.login_view = 'login'
 oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 from app import views, models
